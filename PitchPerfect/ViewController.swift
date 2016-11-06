128364933b1d4c576a36c2164137c882e34a51a1
Deniz Karatay <karatayd@amazon.com>
2016-11-06 14:13:55 +0000

    Add icons for buttons and enable/disable them according to the state

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,11 +26,20 @@ class ViewController: UIViewController {
     @IBAction func RecordAudio(_ sender: AnyObject) {
         print("Record button is pressed")
         recordingLabel.text = "Recording in progress"
-    
+        stopRecordButton.isEnabled = true
+        recordButton.isEnabled = false
     }
 
     @IBAction func stopRecording(_ sender: AnyObject) {
         print("Stop recording button is pressed")
+        recordingLabel.text = "Tap to record"
+        stopRecordButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordButton.isEnabled = false
+        
     }
 }
 
